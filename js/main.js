ea2f36e135891a00237a9869c035672a7f97837f
Mezzias <mezzias@gmail.com>
2025-07-05 14:45:18 +0200

    Initial web structure for Crankus Ferrum

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener(\"DOMContentLoaded\", () => {
+    fetch(\"/api/stats\")
+        .then(res => res.json())
+        .then(data => {
+            const container = document.getElementById(\"stats-container\");
+            data.forEach(stat => {
+                const div = document.createElement(\"div\");
+                div.textContent = ${stat.nombre}: ;
+                container.appendChild(div);
+            });
+        });
+});
