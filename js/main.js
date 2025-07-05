09ba3d2bb216c60de191438487f7d1d08bcf2912
Mezzias <mezzias@gmail.com>
2025-07-05 17:40:12 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 ﻿
 // 👇 CONFIGURA AQUÍ TU PROYECTO
-const { createClient } = supabase;
+
 const supabaseUrl = "https://jeoivdvhdxzqxnbprpim.supabase.co"; // tu URL Supabase
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Implb2l2ZHZoZHh6cXhuYnBycGltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MTAxOTMsImV4cCI6MjA2NzI4NjE5M30.xjUuKrJlAYpWN4V98TMuC3In5oAUuoa1Sg5VzmOr_hs"; // tu clave pública
-const supabaseClient = createClient(supabaseUrl, supabaseKey);
-const { data, error } = await supabaseClient.from("stats").select("*");
+const supabase = (window.supabase || window.Supabase).createClient(supabaseUrl, supabaseKey);
 
 
 
