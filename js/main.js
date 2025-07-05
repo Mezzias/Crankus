9f2ecb7b1af37d123a809b3529a1067305614290
Mezzias <mezzias@gmail.com>
2025-07-05 17:31:37 +0200

    correcciones

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 ﻿// 👇 CONFIGURA AQUÍ TU PROYECTO
 const supabaseUrl = "https://jeoivdvhdxzqxnbprpim.supabase.co"; // tu URL Supabase
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Implb2l2ZHZoZHh6cXhuYnBycGltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MTAxOTMsImV4cCI6MjA2NzI4NjE5M30.xjUuKrJlAYpWN4V98TMuC3In5oAUuoa1Sg5VzmOr_hs"; // tu clave pública
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = (window.supabase || window.Supabase).createClient(supabaseUrl, supabaseKey);
+
 
 
 const personajeId = "crankus_001";
