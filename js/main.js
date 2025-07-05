87483797ac378707bc4a021d3d2308588899b574
Mezzias <mezzias@gmail.com>
2025-07-05 17:47:18 +0200

    main to module 2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 
 const supabaseUrl = "https://jeoivdvhdxzqxnbprpim.supabase.co"; // tu URL Supabase
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Implb2l2ZHZoZHh6cXhuYnBycGltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MTAxOTMsImV4cCI6MjA2NzI4NjE5M30.xjUuKrJlAYpWN4V98TMuC3In5oAUuoa1Sg5VzmOr_hs"; // tu clave pública
-const supabase = (window.supabase || window.Supabase).createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+const { data, error } = await supabase.from("stats").select("*");
 
 
 
